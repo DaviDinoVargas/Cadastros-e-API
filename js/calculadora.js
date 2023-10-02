4d8f772f2fda6f1db1e7bba6786f26524e67fddf
davidinovargas <davidinovargas@gmail.com>
2023-10-02 09:07:13 -0300

    Cadastro e API

---
+++ js/calculadora.js
@@ -0,0 +1,30 @@
+let displayValue = '';
+
+function appendToDisplay(value) {
+    displayValue += value;
+    document.getElementById('display').value = displayValue;
+}
+
+function clearDisplay() {
+    displayValue = '';
+    document.getElementById('display').value = displayValue;
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue);
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
+function calculateSquareRoot() {
+    try {
+        displayValue = Math.sqrt(eval(displayValue));
+        document.getElementById('display').value = displayValue;
+    } catch (error) {
+        document.getElementById('display').value = 'Erro';
+    }
+}
+
---
+++ js/sidebar.js
@@ -0,0 +1,5 @@
+// Adicione este código em script.js
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+});
